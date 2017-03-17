f245a58c26ac769a7d021d40c0892b0e4990ecf6
astrosquid <varcap.cb@gmail.com>
2017-03-17 01:25:50 -0400

    Fixed JS import. Added example for exporting multiple objects to consts.js.

--- consts.js
+++ consts.js
@@ -1,9 +1,20 @@
+// CB: had to pick the plural of a keyword, huh Steve?
+const networkingConstants =
+  {
+      PORT: 4000,
+      // SOCKET_NAME: '/tmp/watcher.sock',
+  };
 
-const consts =
-    {
-        PORT: 3000,
-        // SOCKET_NAME: '/tmp/watcher.sock',
-    };
+const userConstants = 
+	{
+		ADMIN1: 'steve',
+		ADMIN2: 'chris'
+	};
 
 // expose module methods/objects
-exports.consts = consts;
+// CB: if we could stuff multiple objects into this export, then I think it would be fine to call it "consts". 
+// Problem is that currently, the way it had to be referenced in other files was by calling var CONSTANT = networkingConsts
+module.exports = {
+	networkingConsts: networkingConstants,
+	userConsts: userConstants
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-const networkingConsts = require('consts').consts;
-const PORT = networkingConsts.PORT;
+const consts = require('./consts'); // needed to be pointed at a specific file.
+console.log(consts);
+const PORT = consts.networkingConsts.PORT;
+console.log('port is: ' + PORT);
 
 const app = require('express')();
 const http = require('http').Server(app);
@@ -15,11 +17,27 @@ const acceptedusers = {
 const acceptedUsers = ['chris', 'steve'];
 */
 
+/*
+This function is from https://stackoverflow.com/questions/7990890/how-to-implement-login-auth-in-node-js and will be called when we need to see if a user is still logged in. If they aren't, deny access. If they are, bring them to the requested service. To be changed when handling tokens.
+function checkAuth(req, res, next)
+{
+	if (!req.session.user_id) {
+		res.send('Access denied.');
+	} else {
+		next();
+	}
+}
+*/
+
 app.get('/chat', function (req, res)
 {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/login', function (req, res)
+{
+	res.sendFile(__dirname + '/login.html');
+});
 
 io.on('connection', function (socket)
 {
