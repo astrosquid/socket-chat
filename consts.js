7e20ce8efd2b76a718aa5dc7b11e5dc279717553
astrosquid <varcap.cb@gmail.com>
2017-03-26 14:42:05 -0400

    Removed username field and built it into the URL data. Still have to lock down chat using a token received from logging in.

--- consts.js
+++ consts.js
@@ -8,31 +8,34 @@ function define(name, value)
     });
 }
 
+/*
+CB: fails to run because ./constants doesn't exist
 define("PI", 3.14);
 var constants = require("./constants");
 
+CB: no idea why this happens though... 
 console.log(constants.PI); // 3.14
 constants.PI = 5;
 console.log(constants.PI); // still 3.14
-
+*/
 
 // CB: had to pick the plural of a keyword, huh Steve?
 const networkingConstants =
 {
-    PORT: 4000,
-    // SOCKET_NAME: '/tmp/watcher.sock',
+  PORT: 4000,
+  // SOCKET_NAME: '/tmp/watcher.sock',
 };
 
 const userConstants = 
 {
-    ADMIN1: 'steve',
-    ADMIN2: 'chris'
+  ADMIN1: 'steve',
+  ADMIN2: 'chris'
 };
 
 // expose module methods/objects
 // CB: if we could stuff multiple objects into this export, then I think it would be fine to call it "consts". 
 // Problem is that currently, the way it had to be referenced in other files was by calling var CONSTANT = networkingConsts
 module.exports = {
-        networkingConsts: networkingConstants,
-        userConsts: userConstants
+  networkingConsts: networkingConstants,
+  userConsts: userConstants
 };
